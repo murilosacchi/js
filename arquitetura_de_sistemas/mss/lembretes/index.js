59fd82210ea26a84c042245b954021526f120d7d
Murilo Sacchi <144244138+murilosacchi@users.noreply.github.com>
2025-08-25 09:04:47 -0300

    aula 7

---
+++ arquitetura_de_sistemas/mss/lembretes/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+// para obter todos os lembretes
+// GET /lembretes
+app.get('/lembretes', (req, res) => {
+
+})
+
+//para cadastrar novo lembrete
+//POST /lembretes {"texto": "Fazer café"}
+app.post('/lembretes', (req, res) => {
+
+})
+
+const port = 4000
+app.listen(port, () => {
+    console.log(`Lembretes. Porta ${port}`)
+})
